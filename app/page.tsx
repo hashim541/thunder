2388dfde4a82bccd1670ed8195e511007567502c
hashim <hashim00541x@gmail.com>
2024-08-12 12:17:50 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
             </div>
         </nav>
       <section className="flex-1 w-full max-w-[1200px] sm:px-3 px-2 h-full flex flex-col">
-        <section className="w-full flex-1 flex flex-col items-center justify-center">
+        <section className="w-full flex-1 flex flex-col items-center justify-center py-44">
           <h1 className={`${font.head.className}  text-5xl font-bold`}>Don't Re-invent The Wheel</h1>
           <h1 className={`${font.head.className}  text-4xl font-bold`}>Embed Components Faster</h1>
           <p className="text-base mt-5 text-center max-w-4xl">
@@ -42,6 +42,37 @@ export default function Home() {
             </Link>
           </div>
         </section>
+
+        <section className="flex flex-col items-center gap-5 w-full py-20">
+          <h1 className={`${font.head.className}  text-4xl font-bold`}>Comment Component</h1>
+          <div className="w-full flex flex-col items-center">
+            <iframe src="http://localhost:3000/embedded/66b5f99721f0f0c330b78b23/comments/66b5fc1621f0f0c330b78b64?page=1" className="rounded-lg" width="100%" height="500px" ></iframe>  
+            <a href="http://localhost:3000/dashboard/66b5f99721f0f0c330b78b23/comments/66b5fc1621f0f0c330b78b64/add" className="self-center">
+                <button className=" border-2 border-text px-6 py-2 rounded-md font-bold mt-5">Add Your Comment</button>
+            </a>
+          </div>
+        </section>
+
+        <section className="flex flex-col items-center gap-5 w-full py-20">
+          <h1 className={`${font.head.className}  text-4xl font-bold`}>Testimonial Component</h1>
+          <div className="w-full flex flex-col items-center">
+            <iframe src="http://localhost:3000/embedded/66b5f99721f0f0c330b78b23/testimonials/66b73e1a78ee165ef6b63637?page=1"  className="rounded-lg" width="100%" height="500px" ></iframe>  
+            <a href="http://localhost:3000/dashboard/66b5f99721f0f0c330b78b23/testimonials/66b73e1a78ee165ef6b63637/add" className="self-center">
+                <button className=" border-2 border-text px-6 py-2 rounded-md font-bold mt-5">Add Your Testimonial</button>
+            </a>
+          </div>
+        </section>
+
+        <section className="flex flex-col items-center gap-5 w-full py-20">
+          <h1 className={`${font.head.className}  text-4xl font-bold`}>FAQs Component</h1>
+          <div className="w-full flex flex-col items-center">
+            <iframe src="http://localhost:3000/embedded/66b5f99721f0f0c330b78b23/faqs/66b831492ad766df81d2bdb6?page=1" className="rounded-lg" width="100%" height="500px"></iframe>  
+            <a href="http://localhost:3000/dashboard/66b5f99721f0f0c330b78b23/faqs/66b831492ad766df81d2bdb6/add" className="self-center">
+                <button className=" border-2 border-text px-6 py-2 rounded-md font-bold mt-5">Add your question</button>
+            </a>
+          </div>
+        </section>
+
       </section>
     </main>
   );
